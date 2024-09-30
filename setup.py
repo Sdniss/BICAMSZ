32a7d488adc6f70cd6efa73f35f6e95fa6282f9d
sdenisse <stijn.denissen@vub.be>
2024-09-30 17:46:41 +0200

    Update version number for new PyPI release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '1.0.1'
+VERSION = '1.0.2'
 PACKAGE_NAME = 'BICAMSZ'
 AUTHOR = 'Stijn Denissen'
 AUTHOR_EMAIL = 'stijndenissen94@gmail.com'
