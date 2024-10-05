d18d41e64d186d240a52ef1648198a292c35f8ee
sdenisse <stijn.denissen@vub.be>
2024-10-05 21:24:05 +0200

    increase version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '1.0.4'
+VERSION = '1.0.5'
 PACKAGE_NAME = 'BICAMSZ'
 AUTHOR = 'Stijn Denissen'
 AUTHOR_EMAIL = 'stijndenissen94@gmail.com'
